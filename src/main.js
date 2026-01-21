c5655428c95bd27bda556a777ffe329b2ed742c8
TorokD <td.zazsa@gmail.com>
2026-01-21 11:24:57 +0100

    regisztráció elküldés kész

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+function regisztracio() {
+  document.getElementById("alert").innerHTML = "";
+
+  document.getElementById("nevInput").value = "";
+  document.getElementById("emailInput").value = "";
+  document.getElementById("jelszoInput").value = "";
+
+  const alert = document.createElement("div");
+  alert.className = "alert alert-success";
+  alert.textContent = "Sikeres regisztráció";
+  document.getElementById("alert").appendChild(alert);
+}
+
+document.getElementById("elkuldBtn").addEventListener("click",regisztracio)
